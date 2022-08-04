b4b75be5decf44d5e1aca0d661d429eef1e0e2e9
jackhao2018 <chenhao1_vendor@sensetime.com>
2022-08-04 20:21:43 +0800

    音频上传还有点小样式，更多页面需要新增音频和视频的显示

--- templates/test.js
+++ templates/test.js
@@ -1,17 +1,12 @@
-function uploadvideo2(file) {
-    let that=this
-    let FileUrl = URL.createObjectURL(file) //文件转bold流
-    let videoElenment = document.createElement('video');//创建一个视频控件
-    videoElenment.src=FileUrl;//控件视频地址
-    videoElenment.autoplay=true;//是否自动播放
-    videoElenment.addEventListener("canplay",function(){//监听播放
-      let canvas=document.createElement('canvas');//创建一个画板
-      canvas.width=videoElenment.videoWidth * 0.8;//画板宽
-      canvas.height=videoElenment.videoHeight * 0.8;//画板的高
-      //通过画板的drawImage属性对图片进行处理
-      canvas.getContext('2d').drawImage(videoElenment,0,0,canvas.width,canvas.height);
-      //处理完毕通过toDataURL属性转为图片格式拿去base64图片路径
-      let imgs=canvas.toDataURL('image/png')
-      console.log(imgs)//视频第一帧图片路径
-    })
-};
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", '*');
+});
+
+server.on('request', (req, res) => {
+    res.end("SERVER:I received");
+})
+
+server.listen(3000);
+console.log("服务器已启动，监听3000端口，请使用localhost:3000访问");
