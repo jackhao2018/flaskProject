4063d011b937cf0a4fdf46ae54ad0bfd9ff32ee0
Jack2018 <738279242@qq.com>
2022-08-14 23:39:55 +0800

    后台代码整理

--- api/page/home/index.py
+++ api/page/home/index.py
@@ -1,11 +1,15 @@
+import jieba
 from flask import Flask
+from flask import render_template
+from flask import Blueprint
+from stylecloud import stylecloud
+from config.base_config import BASE_DIR
 
-app = Flask(__name__)
-app.config['ENV'] = 'development'
+bp = Blueprint('index', __name__, url_prefix="/")
 
-@app.route('/')
+@bp.route('')
 def world_cloud():
-    with open(r'D:\测试用例数据\textfiles\test-点云.txt', 'r', encoding='utf8') as f:
+    with open( f'{BASE_DIR}/static/test.txt', 'r', encoding='utf8') as f:
         word_list = jieba.cut(f.read())
         result = " ".join(word_list)  # 分词用空格隔开
 
@@ -17,4 +21,10 @@ def world_cloud():
         palette='colorbrewer.diverging.Spectral_11',  # 调色方案选取，从palettable里选择
         gradient='horizontal',  # 渐变色方向选了垂直方向
         icon_name='fas fa-circle',  # 蒙版选取，从Font Awesome里选
-        output_name='../static/img/ciyun.png')  # 输出词云图
+        output_name=f'{BASE_DIR}/static/img/ciyun.png')  # 输出词云图
+    return render_template('index.html')
+
+# @bp.route('/test')
+# def test():
+#     # return 'zheshi ceshi xinxi '
+#     return render_template('index.html')
---
+++ config.py
@@ -0,0 +1,9 @@
+# 设置返回json支持中文
+JSON_AS_ASCII = False
+
+# 设置环境
+ENV = 'development'
+
+DEBUG = True
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
