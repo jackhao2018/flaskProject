6930f65db1e367ab9a25a9efca950a621bdb5184
jackhao2018 <chenhao1_vendor@sensetime.com>
2022-12-08 12:50:00 +0800

    优化图片显示速度

---
+++ static/js/img.js
@@ -0,0 +1,12 @@
+
+$(function(){
+	//一段正则，匹配所有_min.的图片src属性
+	var test = /_min\./
+	//遍历所有的图片节点
+	$("img").each(function(index,obj){
+		if(test.test($(this).attr("src"))){
+			var reSrc = $(this).attr("src").replace(test,".");
+			$(this).attr("src",reSrc)
+		}
+	})
+})
