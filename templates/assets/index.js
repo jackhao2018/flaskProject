f7723ac3c4a450aa278ba4d1c12df960763a6789
jackhao2018 <chenhao1_vendor@sensetime.com>
2022-12-12 12:51:52 +0800

    iframe样式导入待调整

---
+++ templates/assets/index.js
@@ -0,0 +1,8 @@
+$('.tab .menus li').each(function(){
+    $('.tab .menus li').click(function(){
+        $('.tab .menus li').removeClass('bg');
+        $(this).addClass('bg');
+        var index = $(this).index();
+        $('.tab .scroll').css('margin-top',-index*800+'px');
+    })
+})
