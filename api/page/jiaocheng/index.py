a5518ed4b937cbdd68bb60d1fe91f47d4efd5676
jackhao2018 <w738279242@qq.com>
2023-01-07 15:36:53 +0800

    新增ae模块

--- api/page/jiaocheng/index.py
+++ api/page/jiaocheng/index.py
@@ -3,25 +3,25 @@ from models.softwaremodels import SoftwareModel
 import datetime
 from exts import db
 
-
 bp = Blueprint("jiaocheng", __name__, url_prefix="/jiaocheng")
 
 @bp.route("/")
 def index():
     downtype = request.args.get('downtype')
     software_list = []
-    # print(downtype)
     if downtype == 'pr':
         result = SoftwareModel.query.filter(SoftwareModel.softName.like('%' + 'Premiere' + '%')).all()
     elif downtype == 'ps':
         result = SoftwareModel.query.filter(SoftwareModel.softName.like('%' + 'Photoshop' + '%')).all()
+    elif downtype == 'ae':
+        result = SoftwareModel.query.filter(SoftwareModel.softName.like('%' + 'after' + '%')).all()
     else:
         result = SoftwareModel.query.all()
 
     for software in result:
         software_list.append(software.to_dict())
-    return render_template('/jiaocheng/index.html', result={'softwarInfo': software_list, 'total': len(result)})
 
+    return render_template('/jiaocheng/index.html', result={'softwarInfo': software_list, 'total': len(result)})
 @bp.route("/pr/<softname>")
 def prdownload(softname):
     result = SoftwareModel.query.filter(SoftwareModel.softName.like('%' + softname + '%')).first()
@@ -32,14 +32,12 @@ def psdownload(softname):
     result = SoftwareModel.query.filter(SoftwareModel.softName.like('%' + softname + '%')).first()
     return render_template('/jiaocheng/software-download.html', result={"downloadTP": "PS下载", 'data': result.to_dict()})
 
-@bp.route("/ae")
-def aedownload():
 
-    return render_template('/jiaocheng/software-download.html', downloadTp={"type": "AE下载"})
+@bp.route("/ae/<softname>")
+def aedownload(softname):
+    result = SoftwareModel.query.filter(SoftwareModel.softName.like('%' + softname + '%')).first()
+    return render_template('/jiaocheng/software-download.html', result={"downloadTP": "AE下载", 'data': result.to_dict()})
 
 @bp.route("/c4d")
 def c4ddownload():
-
     return render_template('/jiaocheng/software-download.html', downloadTp={"type": "C4D下载"})
-
-
